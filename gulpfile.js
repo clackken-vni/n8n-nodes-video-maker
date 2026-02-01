b4c96c9381ec508f69d9b5b0e3e60462dd305c45
Clackken <hung.dt@hungdang.info>
2026-02-01 20:28:20 +0700

    Fix: Correct n8n package structure with proper dist/ layout

--- gulpfile.js
+++ gulpfile.js
@@ -3,36 +3,23 @@ const zip = require('gulp-zip');
 const path = require('path');
 
 const distPath = path.join(__dirname, 'dist');
-const nodesPath = path.join(distPath, 'nodes');
-const libPath = path.join(distPath, 'lib');
+const nodesPath = path.join(distPath, 'nodes', 'VideoMaker');
+const buildPath = path.join(__dirname, 'build');
 
 function copyNodes() {
-  return gulp
-    .src(`${nodesPath}/**/*`)
-    .pipe(gulp.dest(path.join(__dirname, 'build', 'nodes')));
-}
-
-function copyLib() {
-  return gulp
-    .src(`${libPath}/**/*`)
-    .pipe(gulp.dest(path.join(__dirname, 'build', 'lib')));
+  return gulp.src(nodesPath + '/**/*').pipe(gulp.dest(buildPath + '/nodes/VideoMaker'));
 }
 
 function copyPackage() {
-  return gulp
-    .src(['package.json', 'README.md', 'LICENSE', 'index.js'])
-    .pipe(gulp.dest(path.join(__dirname, 'build')));
+  return gulp.src(['package.json', 'README.md', 'LICENSE', 'index.js']).pipe(gulp.dest(buildPath));
 }
 
 function createZip() {
-  return gulp
-    .src(path.join(__dirname, 'build', '**/*'))
-    .pipe(zip('n8n-nodes-video-maker.zip'))
-    .pipe(gulp.dest(path.join(__dirname, '.')));
+  return gulp.src(buildPath + '/**/*').pipe(zip('n8n-nodes-video-maker.zip')).pipe(gulp.dest(__dirname));
 }
 
 const build = gulp.series(
-  gulp.parallel(copyNodes, copyLib, copyPackage),
+  gulp.parallel(copyNodes, copyPackage),
   createZip
 );
 
